2bf281f662aa0f60e5336bbebd6f3ce9931cd750
Sean Namo <seannamo15@gmail.com>
2024-02-08 03:11:28 +0800

    Remove all module files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
